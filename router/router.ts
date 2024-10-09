749938b25aa351108f530c5b2bf7f3c256325a9e
aein <mananniaein1234@gmail.com>
2024-10-09 20:00:05 +0330

    create json query data

--- router/router.ts
+++ router/router.ts
@@ -3,6 +3,7 @@ import compression from 'compression';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import jsonValueFile from '../start/json-value/json-proj';
+import jsonQuery  from '../start/json-query/json-query';
 
 export default  (app:Application) =>{
    app.use(express.json());
@@ -10,4 +11,5 @@ export default  (app:Application) =>{
    app.use(cors());
    app.use(bodyParser.json());
    app.use("/api/jsonValue" , jsonValueFile);
+   app.use("/api/jsonQuery" , jsonQuery);
 }
---
+++ start/json-query/json-query.ts
@@ -0,0 +1,52 @@
+import {Router} from "express";
+const router = Router();
+import mySqlData from "../db";
+import validateJson from "../../utils/validate-json";
+
+//In the following API, we want to use a Json format
+
+router.post("/" , async (req,res) =>{
+    const {productType,jsonContent} =req.body;
+    //First, we send the following data in postman and then we receive this output from the database
+    // {
+    //     "productType":"Shose",
+    //     "jsonContent":{
+    //         "details":{
+    //             "color":"black",
+    //             "price":786000,
+    //             "size":"43"
+    //         }
+    //     }
+    // }
+
+    const result = await mySqlData(`EXECUTE DataProcessingWithJsonQuery N'${productType}',N'${validateJson(jsonContent)}'`);
+    res.send(result.recordset[0]);
+
+    //Out Put 
+    // {
+    //     "productId": 1,
+    //     "productType": "T-shirt",
+    //     "jsonContent": "{\"details\":{\"color\":\"red\",\"price\":432000,\"size\":\"XXL\"}}",
+    //     "details": "{\"color\":\"red\",\"price\":432000,\"size\":\"XXL\"}"
+    // }
+})
+
+//Well, now we can retrieve the values ​​using jsonValue
+router.get("/" , async(req,res) =>{
+  const {whatColorIsIt,howMuch,WhatSizeIsIt } =req.query;
+
+  const result = await mySqlData(`
+ SELECT 
+		      JSON_VALUE(details,'$.${whatColorIsIt}') AS Color,
+			  JSON_VALUE(details,'$.${howMuch}') AS price,
+			  JSON_VALUE(details,'$.${WhatSizeIsIt}') AS size
+		
+		FROM ProductFetchWithJsonQuery
+    `)
+    res.send(result.recordset[0]);
+
+})
+
+
+
+export default router;
